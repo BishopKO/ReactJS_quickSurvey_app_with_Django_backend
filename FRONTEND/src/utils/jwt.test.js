c0158e97ec7b4064bbd0f86195e300e461079439
BishopKO <42295507+BishopKO@users.noreply.github.com>
2021-06-03 23:50:04 +0100

    MERGE FROM test_charts

--- FRONTEND/src/utils/jwt.test.js
+++ FRONTEND/src/utils/jwt.test.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import axios from 'axios';
+import React from 'react'
+import axios from 'axios'
 
-import { login, sendQueryUsingTokens } from './jwt';
+import { login, sendQueryUsingTokens } from './jwt'
 
-const existinguser = { username: 'bishop', password: 'ghost14' };
+const existing_user = { username: 'bishop', password: 'ghost14' }
 
-describe('API_test', () => {
+describe('API_TEST', () => {
     test('login_and_get_tokens_success', async () => {
-        await login(existinguser.username, existinguser.password).then(response => {
-                console.log(response);
-                expect(response.login).toEqual('SUCCESS');
+        await login(existing_user.username, existing_user.password).then(response => {
+                expect(response.login).toEqual('SUCCESS')
             },
-        );
-    });
+        )
+    })
 
     test('login_and_get_tokens_fail', async () => {
-        const credentials = { username: 'fail', password: 'fail' };
+        const credentials = { username: 'fail', password: 'fail' }
         await login(credentials.username, credentials.password).catch(response => {
-                expect(response.login).toEqual('FAILED');
+                expect(response.login).toEqual('FAILED')
             },
-        );
-    });
+        )
+    })
 })
-;
+
 
